c1df9da511317af4b81a8ea293c43fc3d37762f8
Corey <corey.amano@gmail.com>
2021-07-25 10:16:00 -0700

    update gem file, cors, and add create and show routes

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -3,4 +3,22 @@ class PuppiesController < ApplicationController
     puppies = Puppy.all
     render json: puppies.as_json
   end
+
+  def create
+    puppy = Puppy.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+    )
+    if puppy.save
+      render json: puppy.as_json
+    else
+      render json: { errors: puppy.errors.full_messages }
+    end
+
+    def show
+      pup = Puppy.find_by(id: params[:id])
+      render json: pup.as_json
+    end
+  end
 end
